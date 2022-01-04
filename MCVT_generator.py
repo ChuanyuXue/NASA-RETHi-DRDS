52c365d1ab9a577d85393e0b4cc7f644f0fc6b8a
Chuanyu Xue <chuanyu.xue@uconn.edu>
2022-01-03 19:46:18 -0500

    add fake MCVT script

--- MCVT_generator.py
+++ MCVT_generator.py
@@ -1,40 +1,25 @@
 import api
 import time
+import json
 import random
 
-src_list = [2, 3, 4, 5, 6, 7]
-
-data_id = {
-    2: [6],
-    3: [3, 7],
-    4: [4, 5],
-    5: [8],
-    6: [],
-    7: [],
-}
-
-data_size = {
-    3: 4,
-    4: 50,
-    5: 50,
-    6: 1,
-    7: 1,
-    8: 1,
-}
+with open("db_info.json") as f:
+    data_discript = json.load(f)
 
 # Simulation for 600 seconds
 for synt in range(600):
-    for src in src_list:
+    for name, data in data_discript.items():
         api.init(
             local_ip="127.0.0.1",
             local_port=10002,
             to_ip="127.0.0.1",
-            to_port=10000 + src,
-            client_id=src,
+            to_port=10000 + int(data['data_notes']),
+            client_id=int(data['data_notes']),
             server_id=1
         )
-        for table in data_id[src]:
-            value = [random.random() for i in range(data_size[table])]
-            api.send(synt=synt, id=table, value=value, priority=3, type=1)
+
+        value = [random.random() for i in range(data['data_size'])]
+        api.send(synt=synt, id=data['data_id'],
+                 value=value, priority=3, type=1)
     time.sleep(1)
     print("Simulation time -----------", synt)
